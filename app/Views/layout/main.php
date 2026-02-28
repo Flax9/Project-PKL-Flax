27bab7462353e2133c74d16c90e404c4c59536e0
Flax9 <mahsanulfurqon8@gamil.com>
2026-02-28 20:12:57 +0700

    Mengganti default favicon CI4 dengan logo BPOM

--- app/Views/layout/main.php
+++ app/Views/layout/main.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?? 'Dashboard IKU 2025' ?></title>
+    <link rel="icon" type="image/png" href="<?= base_url('assets/img/logo_bpom_1.png') ?>">
 
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     
